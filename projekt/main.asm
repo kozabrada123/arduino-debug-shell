c0d30eaa28d5f9ffc7c24805dec43718bca95cf5
kozabrada123 <natantriestoedit@gmail.com>
2024-12-10 17:32:22 +0100

    working commands!

--- projekt/main.asm
+++ projekt/main.asm
@@ -10,13 +10,14 @@
 RESET: rjmp setup
 .org 0x0033
 
-// Knjižnica, da ni treba ro?no implementirati serijca
+// Knjižnica, da ni treba rocno implementirati serijca
 .include "knjiznica.asm"
 .include "ascii.asm"
 .include "string_stack.asm"
 
 setup:
 	call setupUART
+	call string_stack_reset
 
 	ldi ZH, high(greet_string<<1)
 	ldi ZL, low(greet_string<<1)
@@ -34,6 +35,17 @@ setup:
 
 	rjmp main
 
+// Main, if debug is enabled
+main_debug:		
+	call get_char
+	// Echo it back in hex
+	call send_hex
+
+	ldi r16, space
+	call send_char
+
+	rjmp main_debug
+
 main:		
 	call get_char
 	call evaluate_character
@@ -43,6 +55,10 @@ main:
 //
 // Character will be in r16
 evaluate_character:
+	// null character
+	cpi r16, 0
+	breq _4
+
 	// Enter
 	cpi r16, enter
 	brne _1
@@ -54,37 +70,126 @@ _1:
 	brne _2
 	// Send space back
 	call send_char
+	// Add a string end to the string stack, so we know there is a break for arguments
+	clr r16
+	call string_stack_push
+	ret
 _2:
 	// Backspace
 	cpi r16, backspace
 	brne _3
+	// Only moves the cursor back
 	call send_char
+	// Remove one from our string buffer
+	call string_stack_pop
 	// Send space and backspace to clear it - dirty hack
 	ldi r16, space
 	call send_char
 	ldi r16, backspace
 	call send_char
+	ret
 _3:
 	call is_r16_alphanumeric
 	cpi r17, 0
 	breq _4
 	call make_r16_lowercase
-	// push to the string stack
 	call string_stack_push
-	// print it back
 	call send_char
+	ret
 _4:
 	ret
 
 // Evaluates / executes the current command
 execute_command:
-	// Push a string end to the string stack
+	// Push a string termination to the string stack
 	clr r16
 	call string_stack_push
-	// TODO: logic
+
+	// Initialize the pointer to the start of the string stack
+	// that's where we store the 0th argument of the command, the command string
+	ldi YH, HIGH(0x0101) // No idea why 0x0100 doesn't work, but this fixes it and it works perfectly now
+	ldi YL, LOW(0x0101)
+
+	// Compare: is it a hello command?
+	ldi ZH, HIGH(hello_command_string << 1)
+	ldi ZL, LOW(hello_command_string  << 1)
+	call string_Y_ram_equals_Z_rom
+	cpi r17, 1
+	breq hello_command
+
+	// Compare: is it a reset command?
+	ldi ZH, HIGH(reset_command_string << 1)
+	ldi ZL, LOW(reset_command_string  << 1)
+	call string_Y_ram_equals_Z_rom
+	cpi r17, 1
+	breq reset_command
+
+	// Compare: is it a version command?
+	ldi ZH, HIGH(version_command_string << 1)
+	ldi ZL, LOW(version_command_string  << 1)
+	call string_Y_ram_equals_Z_rom
+	cpi r17, 1
+	breq version_command
+
+	// Compare: is it a clear command?
+	ldi ZH, HIGH(clear_command_string << 1)
+	ldi ZL, LOW(clear_command_string  << 1)
+	call string_Y_ram_equals_Z_rom
+	cpi r17, 1
+	breq clear_command
+
+	// No other command
+	// Check if it is an empty command
+	ld r16, Y
+	cpi r16, 0
+	breq command_return
+
+	// Print the invalid command text
+	ldi r16, newline
+	call send_char
+	ldi ZH, HIGH(invalid_command_string << 1)
+	ldi ZL, LOW(invalid_command_string  << 1)
+	call printstring
+
+// rjmp here after executing a command
+command_return:
+	// Clear the string buffer
+	call string_stack_reset
 	// Aftermath: print a newline and starting string
 	call print_newline_and_starting
 	ret
+
+// Just prints a message back
+hello_command:
+	ldi r16, newline
+	call send_char
+	ldi ZH, HIGH(hello_command_return_string << 1)
+	ldi ZL, LOW(hello_command_return_string  << 1)
+	call printstring
+	rjmp command_return
+
+// Calls the reset interrupt
+reset_command:
+	rjmp RESET
+
+// Prints the current version
+version_command:
+	ldi r16, newline
+	call send_char
+	ldi ZH, HIGH(version_string << 1)
+	ldi ZL, LOW(version_string  << 1)
+	call printstring
+	rjmp command_return
+
+// ""clears"" the screen
+clear_command:
+	ldi r17, 255
+_clear_loop:
+	ldi r16, newline
+	call send_char
+	dec r17
+	brne _clear_loop
+	rjmp command_return
 
 // Prints a new line and the starting prompt
 print_newline_and_starting:
@@ -94,17 +199,6 @@ print_newline_and_starting:
 	ldi ZL, low(starting_string<<1)
 	call printstring
 	ret
-
-// Main, if debug is enabled
-main_debug:		
-	call get_char
-	// Echo it back in hex
-	call send_hex
-
-	ldi r16, space
-	call send_char
-
-	rjmp main_debug
 
 end:
 	rjmp end
@@ -117,8 +211,30 @@ debug:
 // Strings
 /// Printed when starting the connection
 greet_string:
-	.db "Beep boop, boop beep?", 0
+	.db "| 'Science isn't about why, it's about why not!' | Arduino Debug Shell indev |", 0
+
+version_string:
+	.db "| Arduino Debug Shell | indev | last version change 10/12/24 |", 0
 
 /// Printed after each command, on each new line
-starting_string:				
+starting_string:
 	.db "> ", 0
+
+/// Printed when submitting an invalid command
+invalid_command_string:
+	.db "| Invalid or incomplete command, please try again |", 0
+
+hello_command_string:
+	.db "hello", 0
+
+hello_command_return_string:
+	.db "hi, human!", 0
+
+reset_command_string:
+	.db "reset", 0
+
+version_command_string:
+	.db "version", 0
+
+clear_command_string:
+	.db "clear", 0
--- projekt/string_stack.asm
+++ projekt/string_stack.asm
@@ -6,7 +6,7 @@
  */ 
 
 // Implementation of a home brewed stack, with the X register as a stack pointer
-initialize_string_stack:
+string_stack_reset:
 	ldi XH, HIGH(0x0100)
 	ldi XL, LOW(0x0100)
 
@@ -17,9 +17,54 @@ string_stack_push:
 
 // Pops r16 from the string stack
 string_stack_pop:
-	ld r16, X
-	// FIXME
-	push r16
 	ld r16, -X
-	pop r16
 	ret
+
+// Pushes the string stack pointer (X) to the regular stack
+string_stack_pointer_push:
+	push XH
+	push XL
+
+// Pops the string stack pointer (X) from the regular stack
+string_stack_pointer_pop:
+	pop XL
+	pop XH
+
+// Returns, in r17, whether the string in RAM, starting at where Y is pointing and the string in flash, starting at where Z is pointing, equal
+//
+// Does not modify Y or Z
+string_Y_ram_equals_Z_rom:
+	push YH
+	push YL
+	push ZH
+	push ZL
+	push r16
+	push r18
+_seq_loop:
+	ld r16, Y+ 
+	lpm r18, Z+
+	// Do they equal?
+	cp r16, r18
+	brne _seq_false
+	// Are they 0?
+	cpi r16, 0
+	breq _seq_true
+	rjmp _seq_loop
+
+_seq_false:
+	pop r18
+	pop r16
+	pop ZL
+	pop ZH
+	pop YL
+	pop YH
+	rjmp _set_r17_false_and_ret
+
+_seq_true:
+	pop r18
+	pop r16
+	pop ZL
+	pop ZH
+	pop YL
+	pop YH
+	rjmp _set_r17_true_and_ret
